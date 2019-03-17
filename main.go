22ab73cb6b0144a71b57af5f60944622492312e6
Nikita Nikolenko <nikit011418@gmail.com>
2019-03-17 15:41:00 +0700

    Just hello world

---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "syscall/js"
+
+func main() {
+	println("Hello world!")
+	js.Global().Get("document").Call("getElementById", "runButton").Set("textContent", "Hello from go + wasm!")
+}
